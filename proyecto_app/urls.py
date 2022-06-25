66aedbb0bb43acbe8643948cc70c41d689ae2061
riveramax <maxi.riveram@gmail.com>
2022-06-25 13:41:58 -0300

    commit 25/06
    Creacion login/logout + register

--- proyecto_app/urls.py
+++ proyecto_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from proyecto_app.views import nuevo_descripcion, nuevo_producto, productos, usuarios, descripcion, buscar, nuevo_usuario, nuevo_producto, nuevo_descripcion, Detalle_productos, Borrar_productos, Editar_productos
+from proyecto_app.views import nuevo_descripcion, nuevo_producto, productos, usuarios, descripcion, buscar, nuevo_usuario, nuevo_producto, nuevo_descripcion, Detalle_productos, Borrar_productos, Editar_productos, login_view, logout_view, register_view
 
 urlpatterns = [
     path("productos/", productos, name="productos"),
@@ -14,4 +14,8 @@ urlpatterns = [
     path('detalle-productos/<int:pk>/', Detalle_productos.as_view(), name="detalle_producto"),
     path('borrar-productos/<int:pk>/', Borrar_productos.as_view(), name="borrar_producto"),
     path('editar-productos/<int:pk>/', Editar_productos.as_view(), name="editar_productos"),
+    path('login/', login_view, name = 'login'),
+    path('logout/', logout_view, name = 'logout'),
+    path('register/', register_view, name = 'register'),
+
 ]
--- proyecto_app/views.py
+++ proyecto_app/views.py
@@ -1,11 +1,19 @@
 from urllib import request
-from django.shortcuts import render
+import django
+from django.shortcuts import render, redirect
 from proyecto_app.models import Productos, Usuarios, Descripcion
 from proyecto_app.forms import Usuarios_form, Productos_form, Descripcion_form
 
 from django.views.generic import DetailView, DeleteView, UpdateView
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from proyecto_app.forms import User_registration_form
+
 from django.urls import reverse
+
+
 
 class Detalle_productos(DetailView):
     model = Productos
@@ -116,3 +124,66 @@ def nuevo_descripcion(request):
         return render(request, 'nuevo_descripcion.html', context=context)
 
 
+
+
+#Vista para Login/Logout/Register
+
+def login_view(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                context = {'message':f'Bienvenido {username}!! :D'}
+                return render(request, 'index.html', context = context)
+            else:
+                context = {'errors':'No hay ningun usuario con esas credenciales!!!'}
+                form = AuthenticationForm()
+                return render(request, 'auth/login.html', context = context)
+        else:
+            errors = form.errors
+            form = AuthenticationForm()
+            context = {'errors':errors, 'form':form} 
+            return render(request, 'auth/login.html', context = context)
+
+    else:
+        form = AuthenticationForm()
+        context = {'form':form}
+        return render(request, 'auth/login.html', context = context)
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = User_registration_form(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            context = {'message':f'Usuario creado correctamente {username}'}
+            return render(request, 'index.html', context = context)
+        else:
+            errors = form.errors
+            form = User_registration_form()
+            context = {'errors':errors, 'form':form}
+            return render(request, 'auth/register.html', context = context)
+    else:
+        form = User_registration_form()
+        context = {'form':form}
+        return render(request, 'auth/register.html', context =context)
+
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+def index(request):
+    return render(request, 'index.html')
